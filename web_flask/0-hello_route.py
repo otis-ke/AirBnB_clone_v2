4a410e1a6a2ee709e1c814ef8c7f71802e0c7cd0
otis-ke <vincentotiskisia@gmail.com>
2023-10-18 01:03:14 +0000

    commited

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route('/airbnb-onepage/')
 def hello_hbnb():
     """print web"""
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
+
