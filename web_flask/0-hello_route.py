6000870437254ab88b0541881b096a8a04bb3bd9
otis-ke <vincentotiskisia@gmail.com>
2023-10-19 12:54:35 +0000

    dev

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/airbnb-onepage/')
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """print web"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
